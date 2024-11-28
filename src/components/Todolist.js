2e6ff253123f818073811ffdeba5cbb6a16b71f6
pradumanya <pradumanyaausekar2037@gmail.com>
2024-11-28 22:41:21 +0530

    Initial commit

---
+++ src/components/Todolist.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./todo.css"
+
+
+function Todolist(props) {
+  return (
+    <li className="list-item">
+        {props.item}
+        <span className='icons'>
+        <i className="fa-solid fa-trash-can icon-delete" 
+        onClick={e=>{
+            props.deleteItem(props.index)
+        }}></i>
+
+        </span>
+    </li>
+  )
+}
+
+export default Todolist
